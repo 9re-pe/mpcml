252d847068ea336fadcfd573c9cfbc4cbf6b0f66
t-nakashima-ubuntu <1111yusuke@gmail.com>
2023-10-18 15:26:53 +0900

    experiment

--- modules/searches/Mymp.py
+++ modules/searches/Mymp.py
@@ -1,5 +1,3 @@
-import random
-
 import torch
 
 from .BaseSearch import BaseSearch
@@ -14,6 +12,7 @@ class Mymp(BaseSearch):
     ):
         super().__init__(model)
         self.search_range = search_range
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     def predict(self, pairs: torch.Tensor) -> torch.Tensor:
         """
@@ -23,7 +22,7 @@ class Mymp(BaseSearch):
             MP : MP for each user and item pair size (n_pairs)
         """
         # set users and items
-        user_id  = pairs[:, :1]
+        user_id = pairs[:, :1]
         item_ids = pairs[:, 1:2]
 
         u_emb = self.model.user_embedding(user_id)
@@ -40,8 +39,9 @@ class Mymp(BaseSearch):
             p_user2items[index] = p
 
         p_item2users = torch.zeros_like(distances)
+        user_ids = torch.arange(self.model.n_user).unsqueeze(-1).to(self.device)
+        users_emb = self.model.user_embedding(user_ids)
         for index in top_item_indices:
-            users_emb = self.model.user_embedding(torch.arange(self.model.n_user).unsqueeze(-1))
             item_emb = self.model.item_embedding(item_ids[index])
             distances_item = torch.cdist(users_emb, item_emb).reshape(-1)
             rank = (distances_item <= distances[index]).sum()
