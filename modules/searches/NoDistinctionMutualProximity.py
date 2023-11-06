d2cc5f4ed0801ab333880a913e3a154e3d10c960
9re-pe <1111yusuke@gmail.com>
2023-11-06 17:45:35 +0900

    add no distinction mp

--- modules/searches/NoDistinctionMutualProximity.py
+++ modules/searches/NoDistinctionMutualProximity.py
@@ -7,21 +7,21 @@ from ..models import BaseEmbeddingModel
 from ..distributions.BaseDistribution import BaseDistribution
 
 
-class MutualProximity(BaseSearch):
+class NoDistinctionMutualProximity(BaseSearch):
     def __init__(
             self,
             model: BaseEmbeddingModel,
             distribution: BaseDistribution,
-            n_item_sample: int = 30,
-            n_user_sample: int = 30,
+            n_sample: int = 30,
             bias: float = 0.5
     ):
         super().__init__(model)
         self.distribution = distribution
-        self.n_item_sample = n_item_sample
-        self.n_user_sample = n_user_sample
         self.bias = bias
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+        self.n_user_sample = int(n_sample * (self.model.n_user / (self.model.n_user + self.model.n_item)))
+        self.n_item_sample = n_sample - self.n_user_sample
 
     def compute_users_distribution_params(self, users: torch.Tensor, n_pairs: int):
         """Compute the probability distribution's parameters for users.
@@ -32,17 +32,23 @@ class MutualProximity(BaseSearch):
         Returns:
             params1, params2 : probability distribution's parameters size (n_pairs)
         """
-        # sample items for probability distribution's parameter estimation
+        # sample users and items for probability distribution's parameter estimation
+        sample_users = torch.tensor(self.sample_ids(self.model.n_user, self.n_user_sample)).unsqueeze(1)
         sample_items = torch.tensor(self.sample_ids(self.model.n_item, self.n_item_sample)).unsqueeze(1)
+        sample_users = sample_users.to(self.device)
         sample_items = sample_items.to(self.device)
 
-        i_emb = self.model.item_embedding(sample_items)
-        u_emb = self.model.user_embedding(users)[0].unsqueeze(0).repeat(i_emb.size()[0], 1, 1)
+        u_emb_sampled = self.model.user_embedding(sample_users)
+        i_emb_sampled = self.model.item_embedding(sample_items)
+        sample_emb = torch.cat((u_emb_sampled, i_emb_sampled), dim=0)
 
-        distances = torch.cdist(u_emb, i_emb).reshape(-1)
+        target_u_emb = self.model.user_embedding(users)[0].unsqueeze(0).repeat(sample_emb.size()[0], 1, 1)
+
+        distances = torch.cdist(target_u_emb, sample_emb).reshape(-1)
+
         params = self.distribution.approximate_params(distances)
 
-        # reshape the size to compute MP
+        # Reshape the size to compute MP
         params1 = torch.full((n_pairs,), float(params[0]))
         params2 = torch.full((n_pairs,), float(params[1]))
         params = [params1, params2]
@@ -58,17 +64,22 @@ class MutualProximity(BaseSearch):
         Returns:
             params1, params2 : probability distribution's parameters size (n_pairs)
         """
-        # sample users for probability distribution's parameter estimation
+        # Sample users and items for probability distribution's parameter estimation
         sample_users = torch.zeros((n_pairs, self.n_user_sample), dtype=torch.int64)
+        sample_items = torch.zeros((n_pairs, self.n_item_sample), dtype=torch.int64)
         for i in range(n_pairs):
-            row = self.sample_ids(self.model.n_user, self.n_user_sample)
-            sample_users[i] = torch.tensor(row)
+            sample_users[i] = torch.tensor(self.sample_ids(self.model.n_user, self.n_user_sample))
+            sample_items[i] = torch.tensor(self.sample_ids(self.model.n_item, self.n_item_sample))
         sample_users = sample_users.to(self.device)
+        sample_items = sample_items.to(self.device)
 
-        u_emb = self.model.user_embedding(sample_users)
-        i_emb = self.model.item_embedding(items)
+        u_emb_sampled = self.model.user_embedding(sample_users.view(-1)).view(n_pairs, -1, self.model.n_dim)
+        i_emb_sampled = self.model.item_embedding(sample_items.view(-1)).view(n_pairs, -1, self.model.n_dim)
+        sample_emb = torch.cat((u_emb_sampled, i_emb_sampled), dim=1)
 
-        distances = self.compute_distance(i_emb, u_emb)
+        target_i_emb = self.model.item_embedding(items)
+
+        distances = self.compute_distance(target_i_emb, sample_emb)
         params = self.distribution.approximate_params(distances)
 
         return params
@@ -121,7 +132,7 @@ class MutualProximity(BaseSearch):
     def sample_ids(n_objects: int, n_sample: int):
         """
         Args:
-            n_objects : the number of all user of item
+            n_objects : the number of all user or item
             n_sample : the number of sample for estimate probability distribution's parameter
         Returns:
             samples : the indices of sampled users or items
--- modules/searches/__init__.py
+++ modules/searches/__init__.py
@@ -2,3 +2,4 @@ from .BaseSearch import BaseSearch
 from .NearestNeighborhood import NearestNeighborhood
 from .MutualProximity import MutualProximity
 from .NearMutualProximity import NearMutualProximity
+from .NoDistinctionMutualProximity import NoDistinctionMutualProximity
